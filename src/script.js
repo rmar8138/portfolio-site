e188e9e4fb4bad5cc3eb2de9e4bbb1025a27b327
rmar8138 <ragan.martinez@live.com>
2019-09-18 12:27:09 +1000

    Refactor detectFooterPosition function and add comment

--- src/script.js
+++ src/script.js
@@ -5,8 +5,6 @@ const menuButton = document.getElementById('menuButton');
 
 let invert = true;
 
-menuButton.addEventListener('click', toggleMenu);
-
 function toggleMenu() {
   // add visibility to menu
   menu.classList.toggle('visible');
@@ -32,18 +30,32 @@ function toggleMenu() {
   }
 }
 
-window.addEventListener('scroll', detectFooterPosition);
-
 function detectFooterPosition(e) {
   y = footer.getBoundingClientRect().y;
-  if (y < 34) {
-    // when footer hits navbar, change navbar style
-    if (invert) {
-      navbar.classList.toggle('invert');
-      invert = false;
-    }
-  } else if (y > 34 && !invert) {
-    invert = true;
+
+  /*
+  The invert boolean is used to control the navbar
+  class toggle.
+  
+  The navbar invert class is toggled based on both
+  the y position of the footer and the invert boolean.
+  Invert boolean is initially set to true. Once the navbar
+  hits the footer and if invert is true, invert class is 
+  toggled, and invert boolean is set to false. This 
+  prevents the class from being constantly toggled when
+  scrolling within the footer. When the navbar leaves the 
+  footer, invert class will be toggled when invert is
+  false, and invert boolean is again set to true.
+  */
+
+  if (y < 34 && invert) {
     navbar.classList.toggle('invert');
+    invert = false;
+  } else if (y > 34 && !invert) {
+    navbar.classList.toggle('invert');
+    invert = true;
   }
 }
+
+window.addEventListener('scroll', detectFooterPosition);
+menuButton.addEventListener('click', toggleMenu);
